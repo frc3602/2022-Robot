e1547a5bd6edcf76b9bf8bab316c6802ee3f0b7d
Cody Wellman <zagdrath@member.fsf.org>
2022-02-15 22:02:19 -0500

    Chng victorSPX to TalonSRX & chng motor names

--- src/main/java/frc/robot/subsystems/IndexSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexSubsystem.java
@@ -5,7 +5,7 @@
 package frc.robot.subsystems;
 
 // Phoenix Imports
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 // WPILib Imports
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,9 +15,9 @@ import frc.robot.Constants.Index;
 public class IndexSubsystem extends SubsystemBase {
   public IndexSubsystem() {
     // Creates the motors & controllers and sets the CAN IDs for each one
-    WPI_VictorSPX motorTop = new WPI_VictorSPX(Index.indexMotorTopCANID);
-    WPI_VictorSPX motorBottom = new WPI_VictorSPX(Index.indexMotorBottomCANID);
-    WPI_VictorSPX motorExtra = new WPI_VictorSPX(Index.indexMotorExtraCANID);
+    WPI_TalonSRX indexMotorTop = new WPI_TalonSRX(Index.indexMotorTopCANID);
+    WPI_TalonSRX indexMotorBottom = new WPI_TalonSRX(Index.indexMotorBottomCANID);
+    WPI_TalonSRX indexMotorExtra = new WPI_TalonSRX(Index.indexMotorExtraCANID);
   }
 
   @Override

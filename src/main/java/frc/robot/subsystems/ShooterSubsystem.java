cfdc9ac9b5e37e86a1d0973b005cef1fe965a715
Cody Wellman <zagdrath@member.fsf.org>
2022-02-20 15:08:18 -0500

    testing shooter

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -9,18 +9,21 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 // WPILib Imports
+import frc.robot.OI;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Constants.Shooter;
 
 public class ShooterSubsystem extends SubsystemBase {
+  // Creates the motors & controllers and sets the CAN IDs for each one
+  CANSparkMax shooterMotor = new CANSparkMax(Shooter.shooterMotorCANID, MotorType.kBrushless);
+
   public ShooterSubsystem() {
-    // Creates the motors & controllers and sets the CAN IDs for each one
-    CANSparkMax shooterMotor = new CANSparkMax(Shooter.shooterMotorCANID, MotorType.kBrushless);
   }
 
   public void ShootTest() {
-    
+    shooterMotor.set(OI.joystick.getRawAxis(3));
   }
 
   @Override

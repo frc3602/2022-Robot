1c258447368d9cbbaf1495d74314865213ba1039
Cody Wellman <zagdrath@member.fsf.org>
2022-02-22 09:24:14 -0500

    Fix licenses and files

--- src/main/java/com/frc3602/robot/commands/DriveOrientation.java
+++ src/main/java/com/frc3602/robot/commands/DriveOrientation.java
@@ -1,6 +1,6 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/* Copyright (C) 2022 Team 3602. All rights reserved. */
+/* This work is licensed under the terms of the MIT license */
+/* found in the root directory of this project. */
 
 package com.frc3602.robot.commands;
 
